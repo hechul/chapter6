ca9c89dbb25c1c3348e8e5c8070d5358d53e1c5f
heechul <heechul@DESKTOP-4M6E017>
2022-11-26 14:13:06 +0900

    초기 커밋.

---
+++ chapter6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter6
+{
+    class GenericMethodApp
+    {
+        static void Swap<DataType>(ref DataType x,ref DataType y)
+        {
+            DataType temp;
+            temp = x; x = y; y = temp;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
