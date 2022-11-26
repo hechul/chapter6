53450be2103f3599dd2c56269eaad85fb881cbff
희철 정 <wjdgmlcjf222@naver.com>
2022-11-26 14:46:30 +0900

    1

--- chapter6/3.cs
+++ chapter6/3.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.CodeDom.Compiler;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace chapter6
-{
-    class GenericMethodApp
-    {
-        static void Swap<DataType>(ref DataType x, ref DataType y)
-        {
-            DataType temp;
-            temp = x; x = y; y = temp;
-        }
-    }
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int a = 1, b = 2; double c = 1.5, d = 2.5;
-            Console.WriteLine("Before : a = {0}, b = {1}", a, b);
-            Swap<int>(ref a, ref b);
-            Console.WriteLine("After : a = {0}, b = {1}", a, b);
-        }
-    }
-}
+//namespace chapter6
+//{
+//    class GenericMethodApp
+//    {
+//        static void Swap<DataType>(ref DataType x, ref DataType y)
+//        {
+//            DataType temp;
+//            temp = x; x = y; y = temp;
+//        }
+//    }
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int a = 1, b = 2; double c = 1.5, d = 2.5;
+//            Console.WriteLine("Before : a = {0}, b = {1}", a, b);
+//            Swap<int>(ref a, ref b);
+//            Console.WriteLine("After : a = {0}, b = {1}", a, b);
+//        }
+//    }
+//}
---
+++ chapter6/4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+interface IGenericInterface<T>
+{
+    void SetValue(T x);
+    string GetValueType();
+}
+class GenericClass<T> : IGenericInterface<T>
+{
+    private T value;
+    public void SetValue(T x)
+    {
+        value = x;
+    }
+}
+
+namespace chapter6
+{
+    internal class _4
+    {
+        public static void main(String[] args)
+        {
+            
+        }
+    }
+}
