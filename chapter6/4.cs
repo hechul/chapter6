fae0327eb065745e5217c1bd6850011728d8515c
희철 정 <wjdgmlcjf222@naver.com>
2022-11-27 12:38:39 +0900

    Conditional

--- chapter6/4.cs
+++ chapter6/4.cs
@@ -1,41 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
 
 
-    interface IGenericInterface<T>
-    {
-        void SetValue(T x);
-        string GetValueType();
-    }
-    class GenericClass<T> : IGenericInterface<T>
-    {
-        private T value;
-        public void SetValue(T x)
-        {
-            value = x;
-        }
-        public String GetValueType()
-        {
-            return value.GetType().ToString();
-        }
-    }
+//interface IGenericInterface<T>
+//{
+//    void SetValue(T x);
+//    string GetValueType();
+//}
+//class GenericClass<T> : IGenericInterface<T>
+//{
+//    private T value;
+//    public void SetValue(T x)
+//    {
+//        value = x;
+//    }
+//    public String GetValueType()
+//    {
+//        return value.GetType().ToString();
+//    }
+//}
 
-namespace chapter6
-{
-    internal class _4
-    {
-        public static void main(String[] args)
-        {
-            GenericClass<Int32> gInteger = new GenericClass<Int32>();
-            GenericClass<String> gString = new GenericClass<String>();
-            gInteger.SetValue(10);
-            gString.SetValue("Text");
-            Console.WriteLine(gInteger.GetValueType());
-            Console.WriteLine(gString.GetValueType());
-        }
-    }
-}
+//namespace chapter6
+//{
+//    internal class _4
+//    {
+//        public static void Main(String[] args)
+//        {
+//            GenericClass<Int32> gInteger = new GenericClass<Int32>();
+//            GenericClass<String> gString = new GenericClass<String>();
+//            gInteger.SetValue(10);
+//            gString.SetValue("Text");
+//            Console.WriteLine(gInteger.GetValueType());
+//            Console.WriteLine(gString.GetValueType());
+//        }
+//    }
+//}
---
+++ chapter6/5.cs
@@ -0,0 +1,25 @@
+﻿#define CSHARP
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace chapter6
+{
+    class conditionAttrApp
+    {
+        [Conditional("CSHARP")]
+        public static void CsharpMethod()
+        {
+            Console.WriteLine("In the CSharp Method ...");
+        }
+    }
+    internal class Class1
+    {public static void Main(String[] args)
+        {
+            conditionAttrApp.CsharpMethod();
+        }
+    }
+}
