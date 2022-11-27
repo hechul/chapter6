2779b0a942f7a675b4b8c5df056220f140e8faf1
희철 정 <wjdgmlcjf222@naver.com>
2022-11-27 13:18:19 +0900

    11

---
+++ chapter6/11.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace chapter6
+{
+    class ThreadProperty
+    {
+        public void ThreadBody()
+        {
+            Thread myself = Thread.CurrentThread;
+            for(int i=0; i < 3; i++)
+            {
+                Console.WriteLine("{0} is activated => {1}", myself.Name, i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+    internal class _11
+    {
+        public static void Main(String[] args)
+        {
+            ThreadProperty obj = new ThreadProperty();
+            ThreadStart ts = new ThreadStart(obj.ThreadBody);
+            Thread t1 = new Thread(ts);
+            Thread t2 = new Thread(ts);
+            t1.Name = "Apple"; 
+            t2.Name = "Orange";
+            t1.Start();
+            t2.Start();
+        }
+    }
+}
