32c14c736a38524b4ed4060e754277bc43d575d5
희철 정 <wjdgmlcjf222@naver.com>
2022-11-27 12:46:18 +0900

    6

--- chapter6/5.cs
+++ chapter6/5.cs
@@ -1,33 +1,33 @@
-﻿#define CSHARP
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Diagnostics;
+﻿//#define CSHARP
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Diagnostics;
 
-namespace chapter6
-{
-    class conditionAttrApp
-    {
-        [Conditional("CSHARP")]
-        public static void CsharpMethod()
-        {
-            Console.WriteLine("In the CSharp Method ...");
-        }
-        [Conditional("JAVA")]
-        public static void JAVAMethod()
-        {
-            Console.WriteLine("In the JAVA Method ...");
+//namespace chapter6
+//{
+//    class conditionAttrApp
+//    {
+//        [Conditional("CSHARP")]
+//        public static void CsharpMethod()
+//        {
+//            Console.WriteLine("In the CSharp Method ...");
+//        }
+//        [Conditional("JAVA")]
+//        public static void JAVAMethod()
+//        {
+//            Console.WriteLine("In the JAVA Method ...");
 
-        }
-    }
-    internal class Class1
-    {public static void Main(String[] args)
-        {
-            conditionAttrApp.CsharpMethod();
-            conditionAttrApp.JAVAMethod();
+//        }
+//    }
+//    internal class Class1
+//    {public static void Main(String[] args)
+//        {
+//            conditionAttrApp.CsharpMethod();
+//            conditionAttrApp.JAVAMethod();
 
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ chapter6/6.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter6
+{
+    internal class _6
+    {
+        public static void Main(String[] args)
+        {
+            int i = 1, j = 0, k;
+            k = i / j;
+        }
+    }
+}
