6f0857b2236825cd1a2c38dd1378c46bb0fae45d
희철 정 <wjdgmlcjf222@naver.com>
2022-11-27 12:50:10 +0900

    1

--- chapter6/6.cs
+++ chapter6/6.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace chapter6
-{
-    internal class _6
-    {
-        public static void Main(String[] args)
-        {
-            int i = 1, j = 0, k;
-            k = i / j;
-        }
-    }
-}
+//namespace chapter6
+//{
+//    internal class _6
+//    {
+//        public static void Main(String[] args)
+//        {
+//            int i = 1, j = 0, k;
+//            k = i / j;
+//        }
+//    }
+//}
