7c6c3631bc8d05a5c5bd290068372ff9aaee8377
희철 정 <wjdgmlcjf222@naver.com>
2022-11-27 12:56:16 +0900

    8

--- chapter6/7.cs
+++ chapter6/7.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace chapter6
-{
-    internal class _7
-    {
-        public static void Main(String[] args)
-        {
-            int[] vector = { 1, 2, 3 };
-            try
-            {
-                Console.WriteLine("Here 1");
-                vector[4] = 4;
-                Console.WriteLine("Here 2");
-            }
-            catch (IndexOutOfRangeException)
-            {
-                Console.WriteLine("System Exception is occurred!!!");
-            }
-            Console.WriteLine("Here 3");
-        }
-    }
-}
+//namespace chapter6
+//{
+//    internal class _7
+//    {
+//        public static void Main(String[] args)
+//        {
+//            int[] vector = { 1, 2, 3 };
+//            try
+//            {
+//                Console.WriteLine("Here 1");
+//                vector[4] = 4;
+//                Console.WriteLine("Here 2");
+//            }
+//            catch (IndexOutOfRangeException)
+//            {
+//                Console.WriteLine("System Exception is occurred!!!");
+//            }
+//            Console.WriteLine("Here 3");
+//        }
+//    }
+//}
---
+++ chapter6/8.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace chapter6
+{
+    internal class _8
+    {
+        static int Count = 0;
+
+        public static void Main(String[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    if (++Count == 1) throw new Exception();
+                    if (Count == 3) break;
+                    Console.WriteLine(Count + ") No exception");
+                }catch (Exception)
+                {
+                    Console.WriteLine(Count + ") Exception thrown");
+                }
+                finally
+                {
+                    Console.WriteLine(Count + ") int finally clause");
+
+                }
+            }
+            Console.WriteLine("Main program ends");
+        }
+    }
+}
